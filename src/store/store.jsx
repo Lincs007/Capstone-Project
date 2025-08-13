dae5e403b549840ff2b04a073b61d3078bf4dbca
Lincs <chaza.lincoln@gmail.com>
2025-08-13 12:03:46 +0200

    defined the shoppingCart slice in store.jsx

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productsSlice";
-
+import shoppingCartReducer from "./shoppingCartSlice";
 const store = configureStore({
-  reducer: { products: productReducer },
+  reducer: { products: productReducer, shoppingCart: shoppingCartReducer },
   //middleware: [],
   devTools: process.env.NODE_ENV !== "production",
 });
