7356f8a613b5e8b90deefffb644747ffe79fcf75
Lincs <chaza.lincoln@gmail.com>
2025-08-23 03:33:26 +0200

    create the user details slice and added the logic code

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productsSlice";
 import shoppingCartReducer from "./shoppingCartSlice";
+import userDetailsReducer from "./userDetailsSlice";
+
 const store = configureStore({
-  reducer: { products: productReducer, shoppingCart: shoppingCartReducer },
+  reducer: {
+    products: productReducer,
+    shoppingCart: shoppingCartReducer,
+    userDetails: userDetailsReducer,
+  },
   //middleware: [],
   devTools: process.env.NODE_ENV !== "production",
 });
--- src/store/userDetailsSlice.jsx
+++ src/store/userDetailsSlice.jsx
@@ -9,3 +9,34 @@ const initialState = {
     password: "",
   },
 };
+
+const userDetailsSlice = createSlice({
+  name: "userDetails",
+  initialState,
+  reducers: {
+    saveFirstName: (state, action) => {
+      state.userDetails.firstName = action.payload;
+    },
+    saveSurname: (state, action) => {
+      state.userDetails.surname = action.payload;
+    },
+    saveUsername: (state, action) => {
+      state.userDetails.userName = action.payload;
+    },
+    saveEmail: (state, action) => {
+      state.userDetails.email = action.payload;
+    },
+    savePassword: (state, action) => {
+      state.userDetails.password = action.payload;
+    },
+  },
+});
+
+export const {
+  saveFirstName,
+  saveSurname,
+  saveUsername,
+  saveEmail,
+  savePassword,
+} = userDetailsSlice.actions;
+export default userDetailsSlice.reducer;
