79428067f8e512f1392715c9bac29ec92989ca3f
Lincs <chaza.lincoln@gmail.com>
2025-09-06 14:32:31 +0200

    added an shipping info button with modal properties

--- src/components/shoppingCart.jsx
+++ src/components/shoppingCart.jsx
@@ -1,5 +1,14 @@
-import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Button,
+  Form,
+  Modal,
+} from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import {
   removeFromCart,
   incrementQuantity,
@@ -9,6 +18,9 @@ import {
 function ShoppingCart() {
   const cartProducts = useSelector((state) => state.shoppingCart);
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
   const totalPrice = cartProducts.reduce(
     (total, product) =>
       total +
@@ -91,6 +103,52 @@ function ShoppingCart() {
           <hr />
           <h3> Total:{formatCurrency(totalPrice)} </h3>
           <div className="text-end mt-3">
+            <Row>
+              <Col>
+                <Form.Select
+                  aria-label="shipping-method"
+                  className="shipping-method mb-3"
+                >
+                  <option value="standard">Standard Shipping</option>
+                  <option value="express">Express Shipping</option>
+                  <option value="overnight">Overnight Shipping</option>
+                  <option value="same-day">Same Day Delivery</option>
+                  <option value="pickup">In-Store Pickup</option>
+                </Form.Select>
+              </Col>
+              <Col>
+                <Button
+                  variant="info"
+                  className="me-2"
+                  onClick={openModal}
+                  aria-label="view shipping options info"
+                  style={{ textDecoration: "none" }}
+                >
+                  <i className="bi bi-info-circle text-secondary"></i>"
+                </Button>
+              </Col>
+            </Row>
+            <Modal show={showModal} onHide={closeModal} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>Shipping Information</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p className="shipping-info">
+                  Shipping costs are calculated based on the selected shipping
+                  method and your location. Standard shipping typically takes
+                  3-5 business days, while express shipping takes 1-2 businees
+                  days. Overnight and same-day delivery options are also
+                  available for an additional fee. In-store pickup is free and
+                  allows you to collect your order from our physical store
+                  location at your convenience.
+                </p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="dark" onClick={closeModal}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
             <Button variant="success" size="lg">
               Proceed to Checkout
             </Button>
