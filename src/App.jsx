bd19b75752475442a7ee0a2c460273f05bd719e7
Lincs <chaza.lincoln@gmail.com>
2025-09-16 13:10:35 +0200

    added the routes for all the shop pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import LandingPage from "./components/landingPage";
 import ShoppingCart from "./components/shoppingCart";
 import ProductPage from "./components/productsPage";
+import LoginPage from "./components/loginPage";
+import SignupPage from "./components/signupPage";
 import "./App.css";
 import "./customCss/landingPage.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <LandingPage />
-      <ShoppingCart />
-      <ProductPage />
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
     </>
   );
 }
