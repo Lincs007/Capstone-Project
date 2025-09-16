d29d35905daec93a33696598cd60dac207e920de
Lincs <chaza.lincoln@gmail.com>
2025-09-16 13:09:02 +0200

    create the logic for the navbar

---
+++ src/components/header.jsx
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+import LandingPage from "./landingPage";
+import ProductPage from "./productsPage";
+import ShoppingCart from "./shoppingCart";
+import LoginPage from "./loginPage";
+import SignupPage from "./signupPage";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const cartProducts = useSelector((state) => state.shoppingCart.shoppingCart);
+  const cartQuantity = cartProducts.map((product) => product.quantity);
+  const cartTotalItems = cartQuantity.reduce((total, qty) => total + qty, 0);
+  console.log("Total items in cart:", cartTotalItems);
+  return (
+    <div className="header">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand={true}
+        className="shadow-sm"
+        sticky="top"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to={"/"}>
+            4nez4Mzansi
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/products"}>
+              Products
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/login"}>
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/signup"}>
+              Signup
+            </Nav.Link>
+          </Nav>
+
+          <Nav>
+            <Nav.Link as={Link} to={"/cart"}>
+              Cart{""}
+              <Badge bg="secondary">{cartTotalItems}</Badge>{" "}
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
