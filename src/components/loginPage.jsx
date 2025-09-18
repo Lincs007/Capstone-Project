36dc10729f0788c12ebc699997b3519a7dfc0927
Lincs <chaza.lincoln@gmail.com>
2025-09-18 04:05:57 +0200

    moved the validate logic inside the login component function to avoid errors in the useSelector hook calls

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -2,29 +2,31 @@ import { useFormik } from "formik";
 import { Button, Container, Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
-const validate = (values) => {
-  const errors = {};
-  const savedUser = useSelector((state) => state.userDetails);
-
-  if (!values.userName) {
-    errors.userName = "Username is required";
-  } else if (!values.password) {
-    errors.password = "Password is required";
-  } else {
-    const matchedUser = savedUser.find(
-      (user) => user.userName === values.userName
-    );
-
-    if (!matchedUser) {
-      errors.userName =
-        "There's no existing account linked to the username you provided, please try again or signup to create an account";
-    } else if (matchedUser.password !== values.password) {
-      errors.password =
-        "Wrong password, please try again or click forgot password to start password recovery";
-    }
-  }
-};
 function LoginPage() {
+  const savedUser = useSelector((state) => state.userDetails);
+  console.log("saved user details from redux store:", savedUser);
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.userName) {
+      errors.userName = "Username is required";
+    } else if (!values.password) {
+      errors.password = "Password is required";
+    } else {
+      const matchedUser = savedUser.find(
+        (user) => user.userName === values.userName
+      );
+
+      if (!matchedUser) {
+        errors.userName =
+          "There's no existing account linked to the username you provided, please try again or signup to create an account";
+      } else if (matchedUser.password !== values.password) {
+        errors.password =
+          "Wrong password, please try again or click forgot password to start password recovery";
+      }
+    }
+    return errors;
+  };
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -36,26 +38,24 @@ function LoginPage() {
     },
   });
   return (
-    <Container className="login-container">
+    <Container className="form-container py-5">
       <Form onSubmit={formik.handleSubmit}>
-        <div className="d-flex flex-column align-items-center">
-          <input
+        <Form.Group className="mb-3" controlId="userName">
+          <Form.Control
             name="userName"
-            id="userName"
             type="text"
             placeholder="enter your username"
             onChange={formik.handleChange}
             value={formik.values.userName}
             onBlur={formik.handleBlur}
           />
-          {formik.touched.userName & formik.errors.userName ? (
+          {formik.touched.userName && formik.errors.userName ? (
             <div className="error-message">{formik.errors.userName}</div>
           ) : null}
-        </div>
-        <div className="d-flex flex-column align-items-center">
-          <input
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Control
             name="password"
-            id="password"
             type="password"
             placeholder="enter your password"
             onChange={formik.handleChange}
@@ -65,7 +65,7 @@ function LoginPage() {
           {formik.touched.password && formik.errors.password ? (
             <div className="error-message">{formik.errors.password}</div>
           ) : null}
-        </div>
+        </Form.Group>
         <div className="d-flex flex-column align-items-center">
           <Button variant="dark" type="submit">
             Login
